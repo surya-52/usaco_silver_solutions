a7672333a5a1e5470722dae0c989cee1be90bb60
surya-52 <107560969+surya-52@users.noreply.github.com>
2024-04-19 20:14:43 +0530

    Create paired_up.cpp

---
+++ paired_up.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+using std::mt19937_64;
+using std::random_device;
+using std::uniform_int_distribution;
+#include <numeric>
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             998244353
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+ 
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("pairup.in", "r", stdin);
+    freopen("pairup.out", "w", stdout);
+#endif
+}
+int power(int A, int B)
+{
+    if (B == 0)
+        return 1;
+ 
+    int res = power(A, B / 2);
+ 
+    if (B % 2)
+        return res * res * A;
+    else
+        return res * res;
+}
+
+bool comp(const pair<int,int> &a,const pair<int,int> &b){
+    if(a.second == b.second) return a.first < b.first;
+    return a.second<b.second;
+}
+signed main()
+{   
+    c_p_c();
+    int t = 1;
+    // cin >> t;
+    while(t--){
+      int n;
+      cin >> n;
+      vector<pair<int,int>> v(n);
+      int ans = 0;
+      for(auto &x:v) cin >> x.first >> x.second;
+      sort(v.begin(),v.end(),comp);
+      int i = 0,j = n-1;
+      while(i<=j){
+         if(i==j){
+            ans = max(ans,2*v[i].second);
+            i++;
+            j--;
+            continue;
+         }
+         if(v[i].first > v[j].first){
+            ans = max(ans,v[i].second+v[j].second);
+            v[i].first -= v[j].first;
+            v[j].first = 0;
+            j--;
+         }
+         else if(v[i].first < v[j].first){
+           ans = max(ans,v[i].second+v[j].second);
+            v[j].first -= v[i].first;
+            v[i].first = 0;
+            i++;
+         }
+         else{
+           ans = max(ans,v[i].second+v[j].second);
+           v[j].first = 0;
+           v[i].first = 0;
+           i++;
+           j--;
+         }
+      }
+      cout<<ans<<endl;
+
+
+    }
+  return 0;
+}
